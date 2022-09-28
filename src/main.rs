78be58750da63175092ebee396c77d9ff13f7a35
Yoke <tayok@synspective.com>
2022-09-28 18:04:09 +0900

    modified

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,18 @@ fn query(mut instr: &Instrument, query: &[u8]) -> Result<String, Box<dyn Error>>
     Ok(buf)
 }
 
-fn idn() -> Result<(), Box<dyn Error>> {
-    let rm = DefaultRM::new().unwrap();
-    let rsc = CString::new("TCPIP0::192.168.3.242::hislip0::INSTR")?.into();
+fn idn(visa_address: &str) -> Result<(), Box<dyn Error>> {
+    let rm = DefaultRM::new()?;
+    let rsc = CString::new(visa_address)?.into();
     let instr = rm.open(&rsc, AccessMode::NO_LOCK, TIMEOUT_IMMEDIATE)?;
     let response = query(&instr, b"*IDN?")?;
-    println!("{:?}", response);
+    println!("{:?}", response.trim());
     Ok(())
 }
 
-fn get_screenshot() -> Result<(), Box<dyn Error>> {
-    let rm = DefaultRM::new().unwrap();
-    let rsc = CString::new("TCPIP0::192.168.3.242::hislip0::INSTR")?.into();
+fn get_screenshot(visa_address: &str, filename: &str) -> Result<(), Box<dyn Error>> {
+    let rm = DefaultRM::new()?;
+    let rsc = CString::new(visa_address)?.into();
     let mut instr = rm.open(&rsc, AccessMode::NO_LOCK, TIMEOUT_IMMEDIATE)?;
     instr.write_all(b":HARDcopy:INKSaver OFF")?;
     instr.write_all(b":DISP:DATA? PNG, COLor")?;
@@ -33,19 +33,19 @@ fn get_screenshot() -> Result<(), Box<dyn Error>> {
     while buf.is_empty() {
         let _ = buf_reader.read_to_end(&mut buf);
     }
-    let mut file = File::create("waveform.png")?;
+    let mut file = File::create(filename)?;
     file.write_all(&buf[10..buf.len() - 1])?;
     file.flush()?;
     Ok(())
 }
 
-fn get_csv() -> Result<(), Box<dyn Error>> {
-    let rm = DefaultRM::new().unwrap();
-    let rsc = CString::new("TCPIP0::192.168.3.242::hislip0::INSTR")?.into();
+fn get_csv(visa_address: &str, filename: &str, ch: u64) -> Result<(), Box<dyn Error>> {
+    let rm = DefaultRM::new()?;
+    let rsc = CString::new(visa_address)?.into();
     let mut instr = rm.open(&rsc, AccessMode::NO_LOCK, TIMEOUT_IMMEDIATE)?;
     instr.write_all(b":WAVeform:POINts:MODE RAW")?;
     instr.write_all(b":WAVeform:POINts 10240")?;
-    instr.write_all(b":WAVeform:SOURce CHANnel1")?;
+    instr.write_all(format!(":WAVeform:SOURce CHANnel{}", ch).as_bytes())?;
     instr.write_all(b":WAVeform:FORMat BYTE")?;
 
     let preamble_string = query(&instr, b":WAVeform:PREamble?")?;
@@ -69,7 +69,7 @@ fn get_csv() -> Result<(), Box<dyn Error>> {
         let _ = buf_reader.read_to_end(&mut buf);
     }
 
-    let mut wtr = csv::Writer::from_path("waveform.csv")?;
+    let mut wtr = csv::Writer::from_path(filename)?;
     wtr.write_record(["time", "data"])?;
     for (i, value) in buf[10..buf.len() - 1].iter().enumerate() {
         let time = x_origin + i as f64 * x_increment;
@@ -80,8 +80,10 @@ fn get_csv() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() {
-    idn().unwrap();
-    get_screenshot().unwrap();
-    get_csv().unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let visa_address = "TCPIP0::192.168.3.242::hislip0::INSTR";
+    idn(visa_address)?;
+    get_screenshot(visa_address, "waveform.png")?;
+    get_csv(visa_address, "waveform.csv", 1)?;
+    Ok(())
 }
